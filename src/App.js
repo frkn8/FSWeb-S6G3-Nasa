11f7fb786d7024f7b35d3ab15ff54c0a67004fc0
frkn8 <103068163+frkn8@users.noreply.github.com>
2023-04-01 00:08:02 +0300

    tamamlandı...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
+import BiComponent from "./components/BiComponent";
+function App(){
+const[apodData, setApodData] = useState("asdfas");
+const[datePicker, setDatePicker] = useState("asdfas");
 
-function App() {
+useEffect(() => {
+  
+  axios.get("https://api.nasa.gov/planetary/apod", {
+  params: {
+    api_key: "rNEdFEm1XoNcb1ak5cMltkafZEzVuX4yrnp6KCHr"
+  }
+})
+.then(function (response) {
+  console.log(response);
+  setApodData(response.data);
+})
+.catch(function (error) {
+  console.log(error);
+})
+.finally(function () {
+}); 
+console.log("sayfam render oldu");
+}, [datePicker]);
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+  <BiComponent
+        data={apodData}
+        dateChange={setDatePicker}
+        currentDate={datePicker}
+      />
     </div>
   );
-}
 
+  }
 export default App;
